08a15c468c017295807b241322f9f7eab997ef97
qgq-qiguoqing <18439339670@163.com>
2023-08-01 10:09:54 +0800

    处理一下

--- packages/vite-admin/src/page/main.vue
+++ packages/vite-admin/src/page/main.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="h-full w-full p-3 overflow-hidden" id="iframeBox" ref="rootBox">
     <div
+      id="adminBox"
       :style="{
-        display: isShow.main ? 'block' : 'none',
+        display: 'block',
         width: '100%',
         height: '100%',
         overflow: 'hidden',
@@ -20,7 +21,7 @@
       key="blog"
       id="blog"
       :style="{
-        display: isShow.blog ? 'block' : 'none',
+        display: 'none',
         width: '100%',
         height: '100%',
         overflow: 'hidden',
@@ -31,7 +32,7 @@
       key="admin"
       id="admin"
       :style="{
-        display: isShow.admin ? 'block' : 'none',
+        display: 'none',
         width: '100%',
         height: '100%',
         overflow: 'hidden',
@@ -41,7 +42,7 @@
       key="vuePackage"
       id="vuePackage"
       :style="{
-        display: isShow.vuePackage ? 'block' : 'none',
+        display: 'none',
         width: '100%',
         height: '100%',
         overflow: 'hidden',
@@ -50,101 +51,88 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref, reactive, nextTick, onMounted } from 'vue'
-import { useRoute, useRouter } from 'vue-router'
-import config from '@/assets/config'
-import { navigateToPage } from '@/utils/jumper.ts'
+import { ref, onMounted } from 'vue'
+
+import { dealDom, dealIframe } from '../utils/dealRoute'
 // import { routes } from './router/index.ts'
 
-const router = useRouter()
-const route = useRoute()
-const isShow = reactive<any>({
-  main: true,
-  blog: false,
-  vuePackage: false,
-  admin: false,
-})
+// const router = useRouter()
+// const route = useRoute()
+// const isShow = reactive<any>({
+//   main: true,
+//   blog: false,
+//   vuePackage: false,
+//   admin: false,
+// })
 const rootBox = ref()
 
 // 根据路由跳转 来判断是否需要显示子应用 创建iframe标签后不在销毁 保留在内存中
-function onGo(p: string, v: string, actions?: string) {
-  let url = import.meta.env.DEV ? config[p] : '' // 环境地址
-  let reactBox: any = document.getElementById(p)
-  if (reactBox && !reactBox.children.length) {
-    if (rootBox.value) {
-      var iframe = document.createElement('iframe')
-      iframe.src = url + p + '/' + v
-      iframe.width = rootBox.value?.offsetWidth
-      iframe.height = rootBox.value?.offsetHeight
-      reactBox?.appendChild(iframe)
-      iframe.onload = function () {
-        navigateToPage(
-          p,
-          actions ? '/' + v + '/' + actions : '/' + v,
-          route.query
-        )
-      }
-    }
-  } else {
-    navigateToPage(p, actions ? '/' + v + '/' + actions : '/' + v, route.query)
-  }
-}
-// 不使用watch监听路由变化，使用钩子就需要初始化的时候进行一次手动的跳转
+// function onGo(p: string, v: string, actions?: string) {
+//   let url = import.meta.env.DEV ? config[p] : '' // 环境地址
+//   let reactBox: any = document.getElementById(p)
+//   if (reactBox && !reactBox.children.length) {
+//     if (rootBox.value) {
+//       var iframe = document.createElement('iframe')
+//       iframe.src = url + p + '/' + v
+//       iframe.width = rootBox.value?.offsetWidth
+//       iframe.height = rootBox.value?.offsetHeight
+//       reactBox?.appendChild(iframe)
+//       iframe.onload = function () {
+//         navigateToPage(
+//           p,
+//           actions ? '/' + v + '/' + actions : '/' + v,
+//           route.query
+//         )
+//       }
+//     }
+//   } else {
+//     navigateToPage(p, actions ? '/' + v + '/' + actions : '/' + v, route.query)
+//   }
+// }
+// // 不使用watch监听路由变化，使用钩子就需要初始化的时候进行一次手动的跳转
+// onMounted(() => {
+//   if (route.params.package) {
+//     for (const key in isShow) {
+//       isShow[key] = false
+//     }
+//     let p = route.params.package as string
+//     isShow[p] = true
+//     onGo(
+//       route.params.package as string,
+//       route.params.pathName as string,
+//       route.params.action as string
+//     )
+//   } else {
+//     router.push(route.path)
+//   }
+// })
+// router.beforeEach((v) => {
+//   if (v.params && v.params.pathName) {
+//     for (const key in isShow) {
+//       isShow[key] = false
+//     }
+//     let p = v.params.package as string
+//     isShow[p] = true
+//     console.log(v, '路由信息')
+//     nextTick(() => {
+//       onGo(
+//         v.params.package as string,
+//         v.params.pathName as string,
+//         v.params.action as string
+//       )
+//     })
+//   } else {
+//     for (const key in isShow) {
+//       isShow[key] = false
+//     }
+//     isShow.main = true
+//   }
+// })
+const { addWatch, initialization } = dealIframe(rootBox)
 onMounted(() => {
-  if (route.params.package) {
-    for (const key in isShow) {
-      isShow[key] = false
-    }
-    let p = route.params.package as string
-    isShow[p] = true
-    onGo(
-      route.params.package as string,
-      route.params.pathName as string,
-      route.params.action as string
-    )
-  } else {
-    router.push(route.path)
-  }
+  initialization()
 })
-router.beforeEach((v) => {
-  if (v.params && v.params.pathName) {
-    for (const key in isShow) {
-      isShow[key] = false
-    }
-    let p = v.params.package as string
-    isShow[p] = true
-    console.log(v, '路由信息')
-    nextTick(() => {
-      onGo(
-        v.params.package as string,
-        v.params.pathName as string,
-        v.params.action as string
-      )
-    })
-  } else {
-    for (const key in isShow) {
-      isShow[key] = false
-    }
-    isShow.main = true
-  }
-})
-
+addWatch()
+dealDom(rootBox)
 // 监听页面的尺寸变化动态修改iframe的宽高
-window.addEventListener('resize', () => {
-  // nextTick(() => {
-  if (rootBox.value && rootBox.value.offsetWidth) {
-    let width = rootBox.value.offsetWidth
-    let height = rootBox.value.offsetHeight
-    let iframeBox = document.getElementById('iframeBox')
-    let iframeS = iframeBox?.getElementsByTagName('iframe')
-    if (iframeS) {
-      for (let i = 0; i < iframeS.length; i++) {
-        iframeS[i].width = width
-        iframeS[i].height = height
-      }
-    }
-  }
-
-  // })
-})
 </script>
--- packages/vite-admin/src/utils/dealRoute.ts
+++ packages/vite-admin/src/utils/dealRoute.ts
@@ -1,8 +1,11 @@
-import { ref, nextTick, computed } from 'vue'
+import { ref, nextTick, computed, onMounted } from 'vue'
+import type { Ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { routes } from '@/router'
 import { closeRoute } from '@/utils/jumper.ts'
-
+import config from '@/assets/config'
+import { navigateToPage } from '@/utils/jumper.ts'
+// 处理跳转和记录路由
 export function dealRoute() {
     const router = useRouter()
     const route = useRoute()
@@ -16,7 +19,6 @@ export function dealRoute() {
     }
     // tab栏的操作
     function dealTabs(v: string, parent?: string) {
-
         let n = urls.value.indexOf(v)
         if (n == -1) {
             let children = routes[0].children as any[]
@@ -44,7 +46,6 @@ export function dealRoute() {
             setTabs(n)
         }
     }
-
     function onTab(v: any, i: number) {
         router.push(v.url)
         setTabs(i)
@@ -87,9 +88,7 @@ export function dealRoute() {
             })
             // 收集子应用发过来的路由信息 通知主应用改变浏览器地址
             window.top?.addEventListener('message', (e) => {
-
                 if (e.data.url) {
-
                     let url = e.data.url.includes(e.data.origin)
                         ? e.data.url
                         : '/' + e.data.origin + e.data.url
@@ -112,7 +111,6 @@ export function dealRoute() {
                                 let j = urls.value.indexOf(parentUrl)
 
                                 if (j > -1) {
-
                                     tabs.value.splice(j + 1, 0, {
                                         url: url,
                                         title: e.data.title,
@@ -149,6 +147,10 @@ export function dealRoute() {
             })
         })
     }
+    function clearTab() {
+        tabs.value = []
+        window.localStorage.setItem('tabs', '[]')
+    }
     return {
         tabs,
         route,
@@ -160,6 +162,148 @@ export function dealRoute() {
         onClose,
         setTabs,
         dealTabs,
+        clearTab,
+    }
+}
+export function dealDom(rootBox: Ref<any>, parentDom: string = 'iframeBox') {
+    nextTick(() => {
+        window.addEventListener('resize', () => {
+            // nextTick(() => {
+            if (rootBox.value && rootBox.value.offsetWidth) {
+                let width = rootBox.value.offsetWidth
+                let height = rootBox.value.offsetHeight
+                let iframeBox = document.getElementById(parentDom)
+                let iframeS = iframeBox?.getElementsByTagName('iframe')
+                if (iframeS) {
+                    for (let i = 0; i < iframeS.length; i++) {
+                        iframeS[i].width = width
+                        iframeS[i].height = height
+                    }
+                }
+            }
+        })
+    })
+}
 
+export function dealIframe(
+    rootBox: Ref<any>,
+    parentDom: string = 'iframeBox',
+    mainDom: string = 'adminBox'
+) {
+    const router = useRouter()
+    const route = useRoute()
+    function onGo(p: string, v: string, actions?: string) {
+        let url = import.meta.env.DEV ? config[p] : '' // 环境地址
+        let reactBox: any = document.getElementById(p)
+        if (reactBox && !reactBox.children.length) {
+            if (rootBox.value) {
+                var iframe = document.createElement('iframe')
+                iframe.src = url + p + '/' + v
+                iframe.width = rootBox.value?.offsetWidth
+                iframe.height = rootBox.value?.offsetHeight
+                iframe.id = p
+                reactBox?.appendChild(iframe)
+                iframe.onload = function () {
+                    navigateToPage(
+                        p,
+                        actions ? '/' + v + '/' + actions : '/' + v,
+                        route.query
+                    )
+                }
+            }
+        } else {
+            navigateToPage(
+                p,
+                actions ? '/' + v + '/' + actions : '/' + v,
+                route.query
+            )
+        }
+    }
+    // 不使用watch监听路由变化，使用钩子就需要初始化的时候进行一次手动的跳转
+
+    function addWatch() {
+        nextTick(() => {
+            router.beforeEach(async (v) => {
+                let p = v.params.package as string
+                let iframeBox = document.getElementById(parentDom)
+                let iframeS = iframeBox?.getElementsByTagName('iframe')
+                if (v.params && v.params.pathName) {
+                    await onGo(
+                        v.params.package as string,
+                        v.params.pathName as string,
+                        v.params.action as string
+                    )
+                    const adminBox = document.getElementById(mainDom)
+                    if (adminBox) {
+                        adminBox.style.display = 'none'
+                    }
+                    if (iframeS) {
+                        for (let i = 0; i < iframeS.length; i++) {
+                            let parentNode = iframeS[i].parentElement
+                            if (parentNode) {
+                                if (parentNode.id == p) {
+                                    parentNode.style.display = 'block'
+                                } else {
+                                    parentNode.style.display = 'none'
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    const adminBox = document.getElementById(mainDom)
+                    if (adminBox) {
+                        adminBox.style.display = 'block'
+                    }
+                    if (iframeS) {
+                        for (let i = 0; i < iframeS.length; i++) {
+                            let parentNode = iframeS[i].parentElement
+                            if (parentNode) {
+                                parentNode.style.display = 'none'
+                            }
+                        }
+                    }
+                }
+            })
+        })
+    }
+    async function initialization() {
+        const adminBox = document.getElementById(mainDom)
+        if (route.params.package) {
+            await onGo(
+                route.params.package as string,
+                route.params.pathName as string,
+                route.params.action as string
+            )
+            let iframeBox = document.getElementById(parentDom)
+            let iframeS = iframeBox?.getElementsByTagName('iframe')
+            let p = route.params.package as string
+            if (adminBox) {
+                adminBox.style.display = 'none'
+            }
+
+            if (iframeS) {
+                for (let i = 0; i < iframeS.length; i++) {
+                    let parentNode = iframeS[i].parentElement
+                    if (parentNode) {
+                        console.log(parentNode.id, p)
+                        if (parentNode.id == p) {
+                            parentNode.style.display = 'block'
+                        } else {
+                            parentNode.style.display = 'none'
+                        }
+                    }
+                }
+            }
+        } else {
+            if (adminBox) {
+                adminBox.style.display = 'block'
+            }
+            router.push(route.path)
+        }
+    }
+    return {
+        addWatch,
+        onGo,
+        initialization,
     }
 }
